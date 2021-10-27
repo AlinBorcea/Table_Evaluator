08a20926b28a383d1fd802c46d4bbe38f1b75e97
alin <alinborcea2@gmail.com>
2021-10-27 15:08:41 +0300

    added read_str function

--- src/main.c
+++ src/main.c
@@ -1,6 +1,32 @@
 #include <stdio.h>
+#include <conio.h>
+#include <string.h>
+
+#define BUFFER_SIZE 32
+
+void read_str(char input[BUFFER_SIZE]) {
+    char ch = '0';
+    int i = 0;
+
+    while (i < BUFFER_SIZE - 1) {
+        ch = getch();
+        if (ch == '\r') break;
+
+        printf("%c", ch);
+        input[i] = (char) ch;
+        i++;
+    }
+
+    printf("\n");
+    input[i] = '\0';
+}
 
 int main() {
-    printf("Hello world!asd");
+    char input[BUFFER_SIZE];
+
+    read_str(input);
+    int len = strlen(input);
+    printf("str = %s len = %d\n", input, len);
+
     return 0;
 }
