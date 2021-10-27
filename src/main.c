d9e96cdf53e49d6339f642f9a0e9933ed36c4ddb
alin <alinborcea2@gmail.com>
2021-10-27 15:27:40 +0300

    added main loop

--- src/main.c
+++ src/main.c
@@ -1,8 +1,19 @@
 #include <stdio.h>
 #include <conio.h>
 #include <string.h>
+#include <stdlib.h>
 
 #define BUFFER_SIZE 32
+#define CLS() system("cls")
+
+enum InputOption {
+    EXIT,
+    MENU,
+    SEL,
+    PRINT,
+    READ,
+    CLS,
+};
 
 void read_str(char input[BUFFER_SIZE]) {
     char ch = '0';
@@ -21,12 +32,56 @@ void read_str(char input[BUFFER_SIZE]) {
     input[i] = '\0';
 }
 
-int main() {
-    char input[BUFFER_SIZE];
+void print_menu() {
+    printf("exit -> 0\n");
+    printf("menu -> 1\n");
+    printf("sel -> 2 [row] [column]\n");
+    printf("print -> 3\n");
+    printf("read -> 4\n");
+    printf("clear -> 5\n");
+}
 
-    read_str(input);
-    int len = strlen(input);
-    printf("str = %s len = %d\n", input, len);
+void run_main_loop() {
+    char input[BUFFER_SIZE];
+    int option = MENU;
+    
+    while (option != EXIT) {
+        switch (option) {
+            case EXIT: 
+                break;
+
+            case MENU: 
+                print_menu(); 
+                break;
+
+            case SEL: 
+                break;
+
+            case PRINT: 
+                break;
+
+            case READ: 
+                read_str(input);
+                break;
+
+            case CLS:
+                CLS();
+                break;
+
+            default: 
+                printf("no option %d\n", option); 
+                break;
+        }
+
+        printf("read option\n");
+        option = getch() - 48;
+    }
+
+    CLS();
+}
+
+int main() {
+    run_main_loop();
 
     return 0;
 }
